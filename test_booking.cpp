8e55780e7596a33d8df5a11da9d3b46f960a4648
gem645 <57093145+gem645@users.noreply.github.com>
2023-06-17 16:38:41 +0900

    Create test_booking.cpp

---
+++ test_booking.cpp
@@ -0,0 +1,15 @@
+#include "booking.h"
+#include <assert.h>
+
+int main(){
+    // Simple test case the booking assigns three attributes, booking time of double,
+    // arrival and departure times of type ints.
+    Booking b;
+    b.bookingTime = 14;
+    b.arrivalTime = 15.3;
+    b.departureTime = 18.5;
+    assert (b.bookingTime == 14);
+    assert (b.arrivalTime == 15);
+    assert (b.departureTime == 18);
+    return 0;
+}
